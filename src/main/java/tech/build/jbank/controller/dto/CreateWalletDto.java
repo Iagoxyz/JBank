c1eee2d079553760e9c8d0bd509a6fcdd4f5cab3
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-08-25 13:22:28 -0300

    Dtos

--- src/main/java/tech/build/jbank/controller/dto/CreateWalletDto.java
+++ src/main/java/tech/build/jbank/controller/dto/CreateWalletDto.java
@@ -1,8 +1,12 @@
 package tech.build.jbank.controller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
+
 import java.math.BigDecimal;
 
-public record CreateWalletDto(String cpf,
-                              String email,
-                              String name) {
+public record CreateWalletDto(@CPF @NotBlank String cpf,
+                              @Email @NotBlank String email,
+                              @NotBlank String name) {
 }

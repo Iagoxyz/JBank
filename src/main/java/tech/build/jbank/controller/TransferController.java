df83f3169c15e19326839c4bd740a93f7de02bd5
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-08-26 13:34:47 -0300

    Post - Transfer sender to receiver

---
+++ src/main/java/tech/build/jbank/controller/TransferController.java
@@ -0,0 +1,27 @@
+package tech.build.jbank.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.build.jbank.controller.dto.TransferMoneyDto;
+import tech.build.jbank.service.TransferService;
+
+@RestController
+@RequestMapping(path = "/transfers")
+public class TransferController {
+
+    private final TransferService transferService;
+
+    public TransferController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> transfer(@RequestBody @Valid TransferMoneyDto dto) {
+        transferService.transferMoney(dto);
+        return ResponseEntity.ok().build();
+    }
+}
